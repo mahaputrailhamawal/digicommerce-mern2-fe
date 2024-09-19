2cab92a67071cfc03cb3ce4ef7174332be073bb0
mahaputrailhamawal <mahaputrailhamawal@gmail.com>
2024-09-19 10:03:24 +0700

    add dashboard and dashboard product form

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,3 @@
-import { Mail } from "lucide-react";
-import { Button } from "../components/ui/button";
-import { Tooltip } from "../components/ui/tooltip";
 import { AppRouter } from "./routes";
 
 export default function App() {
